8021d17d679dd657141f7c0f6119b4c1e71a9866
Becky <kye9565@gmail.com>
2020-05-16 17:38:54 +0900

    .

--- python20200322-master/class_Python기초/py08반복문/py08_02_왜반복문을쓰나.py
+++ python20200322-master/class_Python기초/py08반복문/py08_02_왜반복문을쓰나.py
@@ -20,7 +20,7 @@ print("# for 문 종료")
 # while 문을이용하여 "환영합니다"를 다섯번 출력하시오
 print("# while 문 시작")
 i = 0
-while i<5:
+while i < 5:
     print("환영합니다")
-    i = i + 1 # 조건식을 처리하기 위한 문장.
+    i = i + 1  # 조건식을 처리하기 위한 문장.
 print("# while 문 종료")
