2526fe255969a799f6c534c9db6bff9e4eccd877
Becky <kye9565@gmail.com>
2020-05-16 17:43:00 +0900

    .

--- python20200322-master/class_Python기초/py08반복문/py08_03_for문구조.py
+++ python20200322-master/class_Python기초/py08반복문/py08_03_for문구조.py
@@ -1,17 +1,17 @@
-
+# 결과는 열거 처리됨
 # [] : 리스트. 리스트는 배열과 같다.
-# for문
+# for문_리스트
 for name in ["철수", "영희", "길동", "유신"]:
-    print("안녕! " , name)
+    print("안녕! ", name)
 
 
-# for문
+# for문_리스트 안에 리스트
 for x in [1, "a", True, None, ["가"]]:
     print(x, end=",")
 print()
 
 
-# for문
+# for문_문자열
 for x in "abcd faoa":
     print(x, end=",")
 print()
